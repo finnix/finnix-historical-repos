e02f7fca58e5c86002b3b0d0cec6d3366d6bfd57
Ryan Finnie <ryan@finnie.org>
2012-11-19 01:47:14 +0000

    107~4

---
+++ initrd-files/finnix-colors.sh
@@ -0,0 +1,19 @@
+#!/bin/ash
+
+ESC=""
+RESET="]R"
+CRE="[K"
+CLEAR="c"
+NORMAL="[0;39m"
+RED="[1;31m"
+GREEN="[1;32m"
+YELLOW="[1;33m"
+BLUE="[1;34m"
+MAGENTA="[1;35m"
+CYAN="[1;36m"
+WHITE="[1;37m"
+
+# Finnix shortcuts
+BLUEITEM="${WHITE}[${BLUE}*${WHITE}]${NORMAL}"
+YELLOWITEM="${WHITE}[${YELLOW}*${WHITE}]${NORMAL}"
+REDITEM="${WHITE}[${RED}*${WHITE}]${NORMAL}"
