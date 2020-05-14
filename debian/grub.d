50ada043d679b45b0dea1dece19a512e38618c8d
Ryan Finnie <ryan@finnie.org>
2020-05-14 12:08:33 -0700

    Modern updates

--- debian/grub.d
+++ debian/grub.d
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (C) 2012 Ryan Finnie
+# Copyright (C) 2012-2020 Ryan Finnie
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,8 +18,18 @@
 
 set -e
 
-DEFAULTFILE=/etc/default/grub-loopback-iso
-PROGNAME="grub-loopback-iso"
+PROGNAME="loopback-iso"
+
+# Sadly, grub-mkconfig does not export all GRUB_* variables, so we
+# must re-source the default files.
+if test -f /etc/default/grub ; then
+  . /etc/default/grub
+fi
+for x in /etc/default/grub.d/*.cfg ; do
+  if [ -e "${x}" ]; then
+    . "${x}"
+  fi
+done
 
 if [ -f /usr/lib/grub/grub-mkconfig_lib ]; then
   . /usr/lib/grub/grub-mkconfig_lib
@@ -29,43 +39,17 @@ else
   exit 0
 fi
 
-# Read in the location of the ISO
-if [ -f "$DEFAULTFILE" ]; then
-  . "$DEFAULTFILE"
-else
-  exit 0
-fi
-
-# Check if the user has set LOOPBACK_ISO
-[ -n "${LOOPBACK_ISO}" ] || exit 0
+# Configuration
+[ -n "${GRUB_LOOPBACK_ISO}" ] || GRUB_LOOPBACK_ISO="/boot/loopback-iso"
 
 # On the other hand, if set but doesn't exist...
-if ! [ -e "${LOOPBACK_ISO}" ]; then
-  echo "${PROGNAME}: Cannot find ${LOOPBACK_ISO}" >&2
+if ! [ -e "${GRUB_LOOPBACK_ISO}" ]; then
+  echo "${PROGNAME}: Cannot find ${GRUB_LOOPBACK_ISO}" >&2
   exit 0
 fi
 
 process_loopback_iso() {
   iso="$1"
-
-  # Grab some information from the ISO
-  is_valid_loopback=0
-  tempmount="$(mktemp -d)"
-
-  if ! mount -o ro,loop "${iso}" "${tempmount}"; then
-    echo "${PROGNAME}: Cannot mount ${iso}" >&2
-    rmdir "${tempmount}"
-    return
-  fi
-  [ -e "${tempmount}/boot/grub/loopback.cfg" ] && is_valid_loopback=1
-  umount "${tempmount}"
-  rmdir "${tempmount}"
-
-  # Check for error conditions
-  if [ "$is_valid_loopback" = 0 ]; then
-    echo "${PROGNAME}: ${iso} not a valid loopback ISO" >&2
-    return
-  fi
 
   # Get the device path of the ISO's filesystem
   iso_device="$(${grub_probe} -t device ${iso})"
@@ -78,12 +62,7 @@ process_loopback_iso() {
   iso_syspath="$(make_system_path_relative_to_its_root "${iso}")"
   prepare_boot_cache="$(prepare_grub_to_access_device ${iso_device} | sed -e "s/^/\t/")"
 
-  echo "${PROGNAME}: Found loopback ISO" >&2
-  echo "    File: ${iso}" >&2
-  echo "    Device: ${iso_device}" >&2
-  if ! [ "${iso}" = "${iso_syspath}" ]; then
-    echo "    Location on device: ${iso_syspath}" >&2
-  fi
+  echo "Found loopback ISO: ${iso}" >&2
 
   cat << EOF
 menuentry "GRUB Loopback Config (${iso})" {
@@ -102,14 +81,14 @@ EOF
 } # END process_loopback_iso()
 
 # Look for loopback ISOs
-if [ -d "${LOOPBACK_ISO}" ]; then
+if [ -d "${GRUB_LOOPBACK_ISO}" ]; then
   # If it's a directory, process all ISOs found inside
-  for iso in "${LOOPBACK_ISO}"/*.iso; do
+  for iso in "${GRUB_LOOPBACK_ISO}"/*.iso; do
     # Simple way to guard against glob failure (no *.iso in directory)
     [ -e "${iso}" ] || continue
     process_loopback_iso "${iso}"
   done
-else
+elif [ -e "${GRUB_LOOPBACK_ISO}" ]; then
   # If it's a file, just process it
-  process_loopback_iso "${LOOPBACK_ISO}"
+  process_loopback_iso "${GRUB_LOOPBACK_ISO}"
 fi
